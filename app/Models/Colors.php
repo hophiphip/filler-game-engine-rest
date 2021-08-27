5b64fda5bcbf359cd7e5b11a661219e69fe00b9e
hophiphip <nighop@yandex.ru>
2021-08-27 23:30:44 +0400

    Added comments & added Postman collection config

--- app/Models/Colors.php
+++ app/Models/Colors.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
+/**
+ *  This class provides helpful methods for working with colors  
+ */
 final class Colors {
-    // Should match all colors from $colorsTable (case insensitive)
+    /**
+     *  @var string $colorsRegex is the regex string that matches all colors from $colorsTable (case insensitive)
+     */    
     public static $colorsRegex = '/^#([0-9a-f]{6})$|^red$|^green$|^blue$|^yellow$|^magenta$|^cyan$|^white$/i';
 
+    /**
+     *  @var array $colorsTable is a simple colors table to help comparing different representations of colors
+     */
     public static $colorsTable = array(
         '#ff0000' => 0,
         '#00ff00' => 1,
@@ -23,8 +31,10 @@ final class Colors {
            'cyan' => 5,
           'white' => 6,
     );   
-    
 
+    /**
+     * @var array $colors is a color-id to color table
+     */
     public static $colors = array(
         0 => '#ff0000',
         1 => '#00ff00',
@@ -35,10 +45,20 @@ final class Colors {
         6 => '#ffffff',
     );
 
+    /**
+     *  Returns one random color string
+     *  
+     *  @return string
+     */
     public static function randomColorString(): string {
         return self::$colors[random_int(0, count(self::$colors) - 1)];
     }
 
+    /**
+     *  Returns a shuffled array of non-repeating colors.
+     *
+     *  @return array
+     */
     public static function shuffledColors(): array {
         $out = array();
         $out = self::$colors;
@@ -47,6 +67,15 @@ final class Colors {
         return $out;
     }
 
+    /**
+     *  Compares colors. Supports different color representations and is case insensitive.
+     *  Returns 'true' is colors are the same, otherwise 'false'
+     *
+     *  @param string $_r first color to compare
+     *  @param string $_l second color to compare
+     *
+     *  @return bool
+     */
     public static function compareColors(string $_r, string $_l): bool {
         $r = strtolower($_r);
         $l = strtolower($_l);
--- app/Traits/FromNamedArray.php
+++ app/Traits/FromNamedArray.php
@@ -4,6 +4,8 @@ namespace App\Traits;
 
 use Illuminate\Support\Facades\Log;
 
+use App\Models\Cell;
+
 // TODO: But subclasses are still arrays .. update trait to make subclasses accesssible or implement AccessArray trait on Models to prevent possible errors 
 
 // TODO: Mb. fail on missing key -> or add flag to fail on missing key
@@ -13,6 +15,8 @@ trait FromNamedArray {
         foreach (get_object_vars($object = new self) as $property => $default) {
             if (!array_key_exists($property, $data))
                 continue;
+
+            class_uses(Cell::class, true);
 
             $object->{$property} = $data[$property];
         }
